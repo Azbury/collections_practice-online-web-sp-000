a2b6fd76fe23ccdf19a1c1f8ab9d18c6b2912e99
Austin Asbury <acasbury25@aol.com>
2019-08-20 22:28:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,8 +13,5 @@ def sort_array_char_count (array)
 end
 
 def swap_elements (array)
-  temp_ele = array[1]
-  array[2] = array[1]
-  array[1] = temp_ele
-  array
+  array[1], array[2] = array[2], array[1]
 end
